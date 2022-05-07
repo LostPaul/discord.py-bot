ce053392797d21c25ce0a3176bbbf4439bc6da85
Lost Paul <70213368+LostPaul@users.noreply.github.com>
2022-05-07 20:30:15 +0200

    trash code

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(DISCORD_BOT_TOKEN)
